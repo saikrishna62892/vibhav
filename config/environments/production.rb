26a491aec21121e1290eb6ea16ce5141d947e94f
saikrishna62892 <saikrishna62892@gmail.com>
2019-11-21 22:45:51 +0530

    last update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,19 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+config.action_mailer.delivery_method = :smtp
+  host = 'https://vibhavnitc.herokuapp.com/'
+  config.action_mailer.default_url_options = { host:host }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => 'localhost:3000',
+      :user_name => "saikrishna_m190241cs@nitc.ac.in",
+      :password => "07-12-1997",
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
 end
